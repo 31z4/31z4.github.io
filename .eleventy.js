d0c0290be9a175d6a84b3813b81060f787955e20
Elisey Zanko <elisey.zanko@gmail.com>
2024-09-22 18:43:34 +0500

    Apply CSS reset

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ import syntaxHighlight from '@11ty/eleventy-plugin-syntaxhighlight'
 
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight)
-  eleventyConfig.addPassthroughCopy('src/prism.css')
+  eleventyConfig.addPassthroughCopy('src/styles')
 
   return {
     dir: {
